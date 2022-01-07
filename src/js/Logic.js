02485431b610816ed02020bbf4b46fd02d14f01e
Alexander Burachkov <zicio@yandex.ru>
2022-01-07 16:13:20 +0400

    Update Logic.js

--- src/js/Logic.js
+++ src/js/Logic.js
@@ -1,14 +1,17 @@
 export default class Logic {
   constructor(element) {
     this.element = element;
-    this.url = 'https://zicio-heroku.herokuapp.com/?method=';
+    this.url = 'https://zicio-heroku.herokuapp.com/';
 
     this.start();
   }
 
   async start() {
-    const url = `${this.url}allTickets`;
+    const url = `${this.url}?method=allTickets`;
     const response = await fetch(url);
-    console.log(response);
+    if (response.ok) {
+      const tickets = await response.json();
+      console.log(tickets);
+    }
   }
 }
