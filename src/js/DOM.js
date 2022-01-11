b69d3bb597b55b887242d2c768fac4461faa672c
Zicio <zicio@yandex.ru>
2022-01-11 16:17:29 +0400

    Worked

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -6,7 +6,7 @@ export default class DOM {
   static renderTickets(tickets) {
     const ticketBox = document.querySelector('.tickets');
     for (const ticket of tickets) {
-      const newTicket = `<div class="ticket"><input type="checkbox" class="ticket__check" id="${ticket.id}"><span class="ticket__name">${ticket.name}</span><span class="ticket__date"></span><input type="button" class="ticket__change ticket__button" value="&#9998"><input type="button" class="ticket__delete ticket__button" value="X">`;
+      const newTicket = `<div class="ticket"><input type="checkbox" class="ticket__check" id="${ticket.id}"><span class="ticket__name">${ticket.name}</span><span class="ticket__date">${ticket.created}</span><input type="button" class="ticket__change ticket__button" value="&#9998"><input type="button" class="ticket__delete ticket__button" value="X">`;
       ticketBox.insertAdjacentHTML('beforeend', newTicket);
       if (ticket.status) {
         const lastTicketCheck = document.getElementById(ticket.id);
